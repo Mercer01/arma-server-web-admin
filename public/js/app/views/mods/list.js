971875a8b61835659185a2a8ce9955b8b5054185
Lagicrus <Lagicrus@users.noreply.github.com>
2019-01-17 03:33:46 +0000

    mods optimised

--- public/js/app/views/mods/list.js
+++ public/js/app/views/mods/list.js
@@ -1,29 +1,29 @@
 define(function (require) {
 
-  "use strict";
+  'use strict'
 
-  var $                   = require('jquery'),
-      _                   = require('underscore'),
-      Backbone            = require('backbone'),
-      Marionette          = require('marionette'),
-      ListItemView        = require('app/views/mods/list_item'),
-      tpl                 = require('text!tpl/mods/list.html'),
+  var $ = require('jquery'),
+    _ = require('underscore'),
+    Backbone = require('backbone'),
+    Marionette = require('marionette'),
+    ListItemView = require('app/views/mods/list_item'),
+    tpl = require('text!tpl/mods/list.html'),
 
-      template = _.template(tpl);
+    template = _.template(tpl)
 
   return Marionette.CompositeView.extend({
     childView: ListItemView,
-    childViewContainer: "tbody",
+    childViewContainer: 'tbody',
     template: template,
 
     events: {
-      "click #refresh": "refresh",
+      'click #refresh': 'refresh',
     },
 
     refresh: function (event) {
-      event.preventDefault();
+      event.preventDefault()
       $.ajax({
-        url: "/api/mods/refresh",
+        url: '/api/mods/refresh',
         type: 'POST',
         success: function (resp) {
 
@@ -31,7 +31,7 @@ define(function (require) {
         error: function (resp) {
 
         },
-      });
+      })
     },
-  });
-});
+  })
+})
--- public/js/app/views/mods/list_item.js
+++ public/js/app/views/mods/list_item.js
@@ -1,38 +1,38 @@
 define(function (require) {
 
-  "use strict";
+  'use strict'
 
-  var $                   = require('jquery'),
-      _                   = require('underscore'),
-      Backbone            = require('backbone'),
-      Marionette          = require('marionette'),
-      Ladda               = require('ladda'),
-      swal                = require('sweet-alert'),
-      tpl                 = require('text!tpl/mods/list_item.html'),
+  var $ = require('jquery'),
+    _ = require('underscore'),
+    Backbone = require('backbone'),
+    Marionette = require('marionette'),
+    Ladda = require('ladda'),
+    swal = require('sweet-alert'),
+    tpl = require('text!tpl/mods/list_item.html'),
 
-      template = _.template(tpl);
+    template = _.template(tpl)
 
   return Marionette.ItemView.extend({
-    tagName: "tr",
+    tagName: 'tr',
     template: template,
 
     events: {
-      "click .destroy": "deleteMod",
+      'click .destroy': 'deleteMod',
     },
 
     deleteMod: function (event) {
-      var self = this;
+      var self = this
       sweetAlert({
-        title: "Are you sure?",
-        text: "The mod will be deleted from the server!",
-        type: "warning",
-        showCancelButton: true,
-        confirmButtonClass: "btn-danger",
-        confirmButtonText: "Yes, delete it!",
-      },
-      function(){
-        self.model.destroy();
-      });
+          title: 'Are you sure?',
+          text: 'The mod will be deleted from the server!',
+          type: 'warning',
+          showCancelButton: true,
+          confirmButtonClass: 'btn-danger',
+          confirmButtonText: 'Yes, delete it!',
+        },
+        function () {
+          self.model.destroy()
+        })
     },
-  });
-});
+  })
+})
