b8a6528be9fe00670162a6a500ab86aa6b54acd5
Sam Mercer <sam@mercerfamily.org.uk>
2019-01-17 02:53:59 +0000

    work on login not working

---
+++ public/js/login.js
@@ -0,0 +1,24 @@
+
+function login() {
+	let email = document.getElementById("emailinput").innerText
+	let password = document.getElementById("pwdinput").innerText
+	console.log(email,password)
+	fetch("/api/login/",{
+		method: "POST",
+		body: {
+			email: email,
+			password: password
+		}
+
+	}).then(data => {
+			console.log(data)
+			// window.location.href = "/"
+	}).catch(error => {
+		console.error(error)
+	})
+}
+
+
+
+
+document.getElementById("loginbtn").addEventListener("click", login())
