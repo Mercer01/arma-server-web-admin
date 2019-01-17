0f426ecd3c6e130be068e6b7558f4874f62339ea
Sam Mercer <sam@mercerfamily.org.uk>
2019-01-17 02:00:30 +0000

    Auto stash before merge of "master" and "origin/master"

--- routes/login.js
+++ routes/login.js
@@ -1,17 +1,27 @@
-var express = require('express')
+let express = require('express')
 let path = require('path')
 
 module.exports = function (LoginManager) {
-  var router = express.Router()
+  let router = express.Router()
 
   router.post('/register', function (req, res) {
-    var response = LoginManager.register(req, res)
+    let response = LoginManager.register(req, res)
     res.send(response)
   })
 
-  router.post('/', function (req, res) {
-    var response = LoginManager.login(req, res)
-    res.send(response)
+  router.post('/', async (req, res) => {
+    let email = req.body.email
+    let password = req.body.password
+
+    if(email === undefined || password === undefined) {
+
+      res.status(400).send("A username or Password was not specified")
+    } else {
+      LoginManager.login(email,password)
+        .then(console.log(result))
+
+      
+    }
   })
 
   return router
