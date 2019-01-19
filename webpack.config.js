5e2927bf51860710b66956207b73d48fbab7c515
Lagicrus <Lagicrus@users.noreply.github.com>
2019-01-19 21:50:00 +0000

    Update and patch to move software to a more recent patch level rather then the ancient version

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ var webpack = require('webpack')
 module.exports = {
   // Entry point for static analyzer
   entry: path.join(__dirname, 'public', 'js', 'app.js'),
+  mode: 'development',
 
   output: {
     // Where to build results
@@ -41,17 +42,17 @@ module.exports = {
   ],
 
   module: {
-    loaders: [
-      { test: /\.css$/, loaders: ['style-loader', 'css-loader'] },
-      { test: /\.json$/, loaders: ['json-loader'] },
-      { test: /\.png$/, loader: 'url-loader?limit=8192&mimetype=image/png' },
-      { test: /\.jpe?g$/, loader: 'url-loader?limit=8192&mimetype=image/jpg' },
-      { test: /\.gif$/, loader: 'url-loader?limit=8192&mimetype=image/gif' },
-      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=8192&mimetype=image/svg+xml' },
-      { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=8192&mimetype=application/font-woff2' },
-      { test: /\.woff(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=8192&mimetype=application/font-woff' },
-      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=8192&mimetype=application/octet-stream' },
-      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file-loader' }
+    rules: [
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+      { test: /\.json$/, use: ['json-loader'] },
+      { test: /\.png$/, use: 'url-loader?limit=8192&mimetype=image/png' },
+      { test: /\.jpe?g$/, use: 'url-loader?limit=8192&mimetype=image/jpg' },
+      { test: /\.gif$/, use: 'url-loader?limit=8192&mimetype=image/gif' },
+      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, use: 'url-loader?limit=8192&mimetype=image/svg+xml' },
+      { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/, use: 'url-loader?limit=8192&mimetype=application/font-woff2' },
+      { test: /\.woff(\?v=\d+\.\d+\.\d+)?$/, use: 'url-loader?limit=8192&mimetype=application/font-woff' },
+      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, use: 'url-loader?limit=8192&mimetype=application/octet-stream' },
+      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, use: 'file-loader' }
     ]
   },
 
