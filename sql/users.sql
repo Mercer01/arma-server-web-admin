fd019d9829836d09ced2d7a1b94bfd31e603c6d6
Sam Mercer <sam@mercerfamily.org.uk>
2019-01-21 13:29:23 +0000

    update users based on @lagicrus BULLSHIT

--- sql/users.sql
+++ sql/users.sql
@@ -5,6 +5,7 @@ CREATE TABLE `users`
   `last_name`  varchar(100) NOT NULL,
   `email`      varchar(100) NOT NULL,
   `password`   varchar(255) NOT NULL,
+  `signed_in_uuid` varchar(255),
   `created`    datetime     NOT NULL,
   `modified`   datetime     NOT NULL,
   PRIMARY KEY (`id`)
