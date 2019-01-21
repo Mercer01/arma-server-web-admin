47cd1944929340598956f1d4287b42817cf4c39e
Lagicrus <Lagicrus@users.noreply.github.com>
2019-01-21 13:31:22 +0000

    Removed undeed code & Start of servers

--- arma-app/src/Nav_Bar.js
+++ arma-app/src/Nav_Bar.js
@@ -1,8 +1,44 @@
-import React, { Component } from 'react';
-import './Nav_Bar.css';
+import React, { Component } from 'react'
+import './Nav_Bar.css'
 
 class Nav_Bar extends Component {
-  render() {
+  constructor (props) {
+    super(props)
+    this.handleServerTabOpen = this.handleServerTabOpen.bind(this)
+    this.handleServerTabClose = this.handleServerTabClose.bind(this)
+    this.state = { serverTabOpen: false }
+  }
+
+  handleServerTabOpen () {
+    this.setState({ serverTabOpen: true })
+  }
+
+  handleServerTabClose () {
+    this.setState({ serverTabOpen: false })
+  }
+
+  ServerListingCreate () {
+  let server_names = ['Ben', 'Bob', 'Jerry', 'Tim']
+  let server_names_li = server_names.map(function (name) {
+    return <li>{name}</li>
+  })
+  return <ul>{server_names_li} </ul>
+}
+
+  render () {
+    let server_a
+    if (this.state.serverTabOpen) {
+      server_a = <li>
+        <a href="#Servers" id="server_nav_bar" onClick={this.handleServerTabClose}>Servers</a>
+        {this.ServerListingCreate()}
+      </li>
+
+    } else {
+      server_a = <li>
+        <a href="#Servers" id="server_nav_bar" onClick={this.handleServerTabOpen}>Servers</a>
+      </li>
+    }
+
     return (
       <div className="navbar navbar-default navbar-fixed-top" role="navigation">
         <div className="container">
@@ -23,9 +59,7 @@ class Nav_Bar extends Component {
               <li>
                 <a href="#Mods">Mods</a>
               </li>
-              <li>
-                <a href="#Servers" id="server_nav_bar">Servers</a>
-              </li>
+                {server_a}
               <li>
                 <a href="Users">Users</a>
               </li>
@@ -33,8 +67,76 @@ class Nav_Bar extends Component {
           </div>
         </div>
       </div>
-    );
+    )
   }
 }
 
-export default Nav_Bar;
+function UserGreeting (props) {
+  return <h1>Welcome back!</h1>
+}
+
+function GuestGreeting (props) {
+  return <h1>Please sign up.</h1>
+}
+
+function Greeting (props) {
+  const isLoggedIn = props.isLoggedIn
+  if (isLoggedIn) {
+    return <Nav_Bar.ServerListingCreate/>
+    // return <UserGreeting/>
+  }
+  return <GuestGreeting/>
+}
+
+function LoginButton (props) {
+  return (
+    <button onClick={props.onClick}>
+      Login
+    </button>
+  )
+}
+
+function LogoutButton (props) {
+  return (
+    <button onClick={props.onClick}>
+      Logout
+    </button>
+  )
+}
+
+class LoginControl extends React.Component {
+  constructor (props) {
+    super(props)
+    this.handleLoginClick = this.handleLoginClick.bind(this)
+    this.handleLogoutClick = this.handleLogoutClick.bind(this)
+    this.state = { isLoggedIn: false }
+  }
+
+  handleLoginClick () {
+    this.setState({ isLoggedIn: true })
+  }
+
+  handleLogoutClick () {
+    this.setState({ isLoggedIn: false })
+  }
+
+  render () {
+    const isLoggedIn = this.state.isLoggedIn
+    let button
+
+    if (isLoggedIn) {
+      button = <LogoutButton onClick={this.handleLogoutClick}/>
+    } else {
+      button = <LoginButton onClick={this.handleLoginClick}/>
+    }
+
+    return (
+      <div>
+        <Greeting isLoggedIn={isLoggedIn}/>
+        {button}
+      </div>
+    )
+  }
+}
+
+export { Nav_Bar, Greeting, LoginControl }
--- arma-app/src/index.js
+++ arma-app/src/index.js
@@ -1,13 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import Nav_Bar from './Nav_Bar'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
+import { Greeting, LoginControl, Nav_Bar } from './Nav_Bar'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(<Nav_Bar />, document.getElementById("nav-bar-root"))
+ReactDOM.render(<App/>, document.getElementById('root'))
+ReactDOM.render(<Nav_Bar/>, document.getElementById('nav-bar-root'))
+ReactDOM.render(
+  <Greeting isLoggedIn={false}/>,
+  document.getElementById('root')
+)
+ReactDOM.render(
+  <LoginControl/>,
+  document.getElementById('root')
+)
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
